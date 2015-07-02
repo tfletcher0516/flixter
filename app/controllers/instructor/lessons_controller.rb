61203b0f54eaa9858773468c23bfeda2b7207f8b
Tim Fletcher <fletcher0516@gmail.com>
2015-07-02 23:04:14 +0000

    added permissions to instructor sessions and lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,16 +1,25 @@
 class Instructor::LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_session
 	def new
-		@section = Section.find(params[:section_id])
+			if current_section.course.user != current_user
+				return render :text => 'Unauthorized', :status => :unauthorized
+			end
 		@lesson = Lesson.new
 	end
 
 	def create
-		@section = Section.find(params[:section_id])
-		@lesson = @section.lessons.create(lesson_params)
-		redirect_to instructor_course_path(@section.course)
+		if current_section.course.user != current_user
+			return render :text=> 'Unauthorized', :status => :unauthorized
+		end
+		@lesson = current_section.lessons.create(lesson_params)
+		redirect_to instructor_course_path(current_section.course)
 	end
 
 	private
+	helper_method :current_section
+	def current_section
+		@current_section ||= Section.find(:section_id)
 
 	def lesson_params
 		params.require(:lesson).permit(:title, :subtitle)
