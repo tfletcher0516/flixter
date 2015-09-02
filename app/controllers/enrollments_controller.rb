a0e8286e73ced73ca9aae2ae241cea66e86af600
Tim Fletcher <fletcher0516@gmail.com>
2015-09-02 17:42:51 +0000

    Changed to process payments and show enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,30 +2,31 @@ class EnrollmentsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		current_user.enrollments.create(:course => current_course)
+    if current_course.premium?
+         # Amount in cents
+      @amount = (current_course.cost * 100).to_i
 
+      customer = Stripe::Customer.create(
+        :email => current_user.email,
+        :card  => params[:stripeToken]
+      )
 
-     # Amount in cents
-  @amount = (current_course.cost * 100).to_i
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Flixter Premium Content',
+        :currency    => 'usd'
+      )
+    end
 
-  customer = Stripe::Customer.create(
-    :email => current_user.email,
-    :card  => params[:stripeToken]
-  )
+    current_user.enrollments.create(:course => current_course)
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Flixter Premium Content',
-    :currency    => 'usd'
-  )
+    redirect_to course_path(current_course)
 
-  redirect_to course_path(current_course)
-
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to root_path
-	end
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to root_path
+  	end
 
 
 
